1ab09efa1b8429d471d843a943f57927ea187d04
Zach Wade <zacharywade@gmail.com>
2015-02-28 17:56:13 -0500

    errything

--- crx/content.js
+++ crx/content.js
@@ -1,5 +1,85 @@
+
+
+/* This is a prototype */
+var createSnackbar = (function() {
+	// Any snackbar that is already shown
+	var previous = null;
+	
+/*
+<div class="paper-snackbar">
+	<button class="action">Dismiss</button>
+	This is a longer message that won't fit on one line. It is, inevitably, quite a boring thing. Hopefully it is still useful.
+</div>
+*/
+	
+	return function(message, actionText, action) {
+		if (previous) {
+			previous.dismiss();
+		}
+		var snackbar = document.createElement('div');
+		snackbar.className = 'paper-snackbar';
+		snackbar.dismiss = function() {
+			this.style.opacity = 0;
+		};
+		var text = document.createTextNode(message);
+		snackbar.appendChild(text);
+		if (actionText) {
+			if (!action) {
+				action = snackbar.dismiss.bind(snackbar);
+			}
+			var actionButton = document.createElement('button');
+			actionButton.className = 'action';
+			actionButton.innerHTML = actionText;
+			actionButton.addEventListener('click', action);
+			snackbar.appendChild(actionButton);
+		}
+		setTimeout(function() {
+			if (previous === this) {
+				previous.dismiss();
+			}
+		}.bind(snackbar), 5000);
+		
+		snackbar.addEventListener('transitionend', function(event, elapsed) {
+			if (event.propertyName === 'opacity' && this.style.opacity == 0) {
+				this.parentElement.removeChild(this);
+				if (previous === this) {
+					previous = null;
+				}
+			}
+		}.bind(snackbar));
+
+		
+		
+		previous = snackbar;
+		document.body.appendChild(snackbar);
+		// In order for the animations to trigger, I have to force the original style to be computed, and then change it.
+		getComputedStyle(snackbar).bottom;
+		snackbar.style.bottom = '0px';
+		snackbar.style.opacity = 1;
+	};
+})();
 var worker = new Worker(chrome.runtime.getURL('worker.js'));
 worker.postMessage(document.body.innerText);
 worker.onmessage = function(event) {
+	createSnackbar("Page Saved");
 	console.log(event.data);
+	var bms = chrome.storage.sync.get("pages",function(val) {
+		var pages = []
+		if (val.pages) {
+			pages = val.pages
+		}
+		console.log(pages)
+		var loc = location.href.split("://")[1].split("#")[0].split("?")[0];
+		if (pages.indexOf(loc) < 0) {
+			pages.push(loc)
+			console.log(chrome.storage.sync.set({"pages": pages}));
+			var a = {}
+			a[loc] = JSON.parse(event.data)
+			console.log(chrome.storage.sync.set(a));
+		}
+	})
 };
+
+spawnLightbox = function() {
+
+}
--- crx/handleReq.js
+++ crx/handleReq.js
@@ -1,4 +1,6 @@
-$("#save").click(function() {
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.insertCSS({"file":"snack.css"})
+	chrome.tabs.insertCSS({"file":"fonts.css"})
 	chrome.tabs.executeScript({"file":"patch-worker.js"});
 	chrome.tabs.executeScript({"file":"content.js"});
 })
