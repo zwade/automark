aef85296a3fb8a2081a5cd668e8c59deca196510
Ellis <el1t@ymail.com>
2015-02-28 18:14:11 -0500

    Advance website

--- public/js/setup-material.js
+++ public/js/setup-material.js
@@ -1,1 +1,10 @@
-$(".button-collapse").sideNav();
+$('.button-collapse').sideNav();
+$('div a.btn-floating').click(function() {
+	toast('hey!', 1000);
+});
+$('div.card.small').click(function() {
+	toast('yo', 400);
+});
+$('#settings').click(function() {
+	$('#modal-settings').openModal();
+});
