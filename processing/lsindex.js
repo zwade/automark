2c1cf71e7f615d23775a065d7ff25222c0e87814
AzurNova <lucas.lin177@gmail.com>
2015-02-28 17:24:09 -0500

    basic lsi and query functionality

--- processing/lsindex.js
+++ processing/lsindex.js
@@ -5,10 +5,77 @@
 var indexer = (function () {
 
 	// Private variables and functions
+	var approxRank;
 	var keywords;
 	var contentmatrix; //Stores by column
+	var rkapproxuk;
+	var rkapproxsk;
+	var docvectors;
 	var docDict;
 
+	// Create the indexing matrices
+	var lsi = function () {
+		var svd = numeric.svd(numeric.transpose(contentmatrix));
+		var uk = numeric.transpose(numeric.transpose(svd.U).slice(0, approxRank));
+		var sk = numeric.diag(svd.S.slice(0, approxRank));
+		var vt = numeric.transpose(numeric.transpose(svd.V).slice(0, approxRank));
+		rkapproxuk = uk;
+		rkapproxsk = sk;
+		docvectors = vt;
+	};
+
+	// Query with keywords
+	var query = function (querywords, numtoreturn) {
+		var ovector = getQueryVector(querywords);
+		var tvector = numeric.dot(numeric.dot(ovector, rkapproxuk), numeric.inv(rkapproxsk));
+		var dists = [];
+		for (var i = 0; i < docvectors.length; i++) {
+			dists.push([i, cosim(tvector, docvectors[i])]);
+		}
+		var sorted = getClosestDocs(dists, numtoreturn);
+		var docs = [];
+		for (var j = 0; j < sorted.length; j++) {
+			docs.push([docDict[sorted[j][0]], sorted[j][1]]);
+		}
+		return docs;
+	};
+
+	var getQueryVector = function (querywords) {
+		var newVector = [];
+		for (var k = 0; k < keywords.length; k++) {
+			newVector.push(0);
+		}
+		for (var j = 0; j < querywords.length; j++) {
+			newVector[binaryIndexOf(querywords[j])] = 1;
+		}
+		return newVector;
+	};
+
+	var cosim = function (v1, v2) {
+		return numeric.dot(v1, v2)/(mag(v1)*mag(v2));
+	};
+
+	var mag = function (v) {
+		var out = 0;
+		for (var i = 0; i < v.length; i++) {
+			out = out + Math.pow(v[i], 2);
+		}
+		return Math.pow(out, 1/2);
+	};
+
+	var getClosestDocs = function (dists, numtoreturn) {
+		dists.sort(function (a, b) {
+			if (a[1] == b[1]) {
+				return 0;
+			}
+			else {
+				return (a[1] > b[1]) ? -1 : 1;
+			}
+		});
+		return dists.slice(0, numtoreturn);
+	};
+
+	// Add a new document to content matrix
 	var addDocument = function (id, docwords, weights) {
 		for (var i = 0; i < docwords.length; i++) {
 			if (keywords.indexOf(docwords[i]) == -1) {
@@ -103,7 +170,7 @@ var indexer = (function () {
 	};
 
 	var rowSwap = function (r1, r2) {
-		for (var i = 0; i < indexer.length; i++) {
+		for (var i = 0; i < contentmatrix.length; i++) {
 			var temp = contentmatrix[i][r1];
 			contentmatrix[i][r1] = contentmatrix[i][r2];
 			contentmatrix[i][r2] = temp;
@@ -115,6 +182,13 @@ var indexer = (function () {
 		keywords = [];
 		contentmatrix = [];
 		docDict = {};
+		approxRank = 2; // Defaults to 2;
+	};
+
+	// Alter Settings
+	var changeApproxRank = function (rank) {
+		approxRank = rank;
+		lsi();
 	};
 
 	// Debugging
@@ -123,17 +197,23 @@ var indexer = (function () {
 	};
 
 	var printContentmatrix = function () {
-		for (var i = 0; i < contentmatrix.length; i++) {
-			console.log(contentmatrix[i].toString());
-		}
+		console.log(contentmatrix);
+	};
+
+	var printDocVectors = function () {
+		console.log(docvectors);
 	};
 
 	// Prototype
 	indexer.prototype = {
 		constructor: indexer,
 		add: addDocument,
+		lsi: lsi,
+		query: query,
+		changeApproxRank: changeApproxRank,
 		printKeywords: printKeywords,
-		printContentmatrix: printContentmatrix
+		printContentmatrix: printContentmatrix,
+		printDocVectors: printDocVectors
 	};
 
 	return indexer;
