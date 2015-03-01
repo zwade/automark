c55fe193c05fddaec339ddebcd7df2b3b24af8b0
Zach Wade <zacharywade@gmail.com>
2015-03-01 00:35:57 -0500

    search is more or less working

--- public/js/lshandle.js
+++ public/js/lshandle.js
@@ -1,6 +1,7 @@
 
 $(window).load(function() {
 	i = new indexer();
+	cards = {};
 	chrome.storage.sync.get(function(p) {
 		chrome.storage.local.get(function(urls) {
 			console.log(p.pages);
@@ -18,12 +19,27 @@ $(window).load(function() {
 				console.log(weights)
 				i.add(page[1],terms,weights)
 				addCard(page[1],urls[p.pages[k]],"http://"+p.pages[k])
+				cards[page[1]] = [urls[p.pages[k]],"http://"+p.pages[k]]
 				
 			}
 			i.lsi();
 			$("#search-term").keypress(function(e) {
-				var c = i.query($("#search-term").val().split(" "));
-				
+				if (e.keyCode == 32 || e.keyCode == 13) {
+					var c = i.query($("#search-term").val().split(" "));
+					removeCards();
+					for (var t in c) {
+						var v = c[t];
+						if (v[1] > 0.25) {
+							addCard(v[0],cards[v[0]][0],cards[v[0]][1]);
+						}
+					}	
+				}
+			})
+			$(".mdi-action-search").click(function(e) {
+				removeCards();
+				for (var t in cards) {
+					addCard(t,cards[t][0],cards[t][1]);
+				}
 			})
 		})
 	})
