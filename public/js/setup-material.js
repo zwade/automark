7e111ea369957c6654bb72a13e3bdce9b995cd28
Ellis <el1t@ymail.com>
2015-02-28 23:07:10 -0500

    Delight the user

--- public/js/setup-material.js
+++ public/js/setup-material.js
@@ -1,9 +1,31 @@
+var searchIcon = $('#search'),
+	searchTerm = $('#search-term');
+
 $('.button-collapse').sideNav();
 $('div a.btn-floating').click(function() {
 	$('#modal-add').openModal();
 });
 $('#settings').click(function() {
 	$('#modal-settings').openModal();
+});
+searchTerm.change(function() {
+	//filter cards
+});
+searchIcon.children().first().click(function() {
+	if (searchIcon.hasClass('active')) {
+		searchTerm.blur();
+		setTimeout(function() {
+			searchIcon.removeClass('active');
+		}, 100);
+		setTimeout(function() {
+			searchTerm.val('');
+		}, 500);
+	} else {
+		searchIcon.addClass('active');
+		setTimeout(function() {
+			searchTerm.focus();
+		}, 300);
+	}
 });
 // May be deleted
 $('.card').scrollfire({
