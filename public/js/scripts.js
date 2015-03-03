8748afd7c4374c1dca3b04ecf188ffbc43d32f80
Ellis <el1t@ymail.com>
2015-03-03 11:42:38 -0500

    Add import modal
    Needs import code, insert at comment position

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,6 +5,7 @@ var columns = [$('#col1'), $('#col2'), $('#col3')],
 	saveModal = $('#save-bookmark'),
 	saveModalContainer = $('#modal-add'),
 	saveModalProgress = saveModalContainer.find('.progress'),
+	importModal = $('#modal-import'),
 	tabs = $('.tabs'),
 	datePicker = $('.datepicker'),
 	urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.!\/\\\w]*))?)/g,
@@ -51,8 +52,8 @@ var addCards = function(lst) {
 var addCategory = function(title,pop) {
 	$("#"+pop).append('<li><a class=\''+pop+'\' type=\''+title+'\' href=\'#'+title+'\'>' + title + '</a></li>');
 	/*categories.children().last().click(function() {
-		toast($(this).text());
-	});*/
+	 toast($(this).text());
+	 });*/
 };
 var removeCards = function() {
 	numCards = 0;
@@ -67,7 +68,6 @@ var validateURL = function() {
 		saveModal.addClass('disabled');
 	}
 };
-
 bookmarkURL[0].oninput = validateURL;
 saveModal.click(function() {
 	if (!saveModal.hasClass('disabled')) {
@@ -77,52 +77,46 @@ saveModal.click(function() {
 		if (!input.match(/http:\/\//) && !input.match(/https:\/\//)) {
 			input = 'http://' + input;
 		}
-		var xhr = new XMLHttpRequest()
-		console.log(input)
-		xhr.open("GET",input)
-		xhr.onerror = function(e) {console.log(e)}
+		var xhr = new XMLHttpRequest();
+		console.log(input);
+		xhr.open("GET",input);
+		xhr.onerror = function(e) {console.log(e)};
 		xhr.onload = function() {
-
-
-var worker = new Worker(chrome.runtime.getURL('crx/worker.js'));
-var type = "s" //allows for other types
-var title = xhr.response.match(/<title>(.+)<\/title>/)
-worker.postMessage(JSON.stringify([xhr.response.split("<body")[1].split("</body")[0].replace(/<[^>]*>/g, "").match(/\w+/g).join(" "),title ? title[1] : input,1.1]));
-worker.onmessage = function(event) {
-	console.log(event.data);
-	var bms = chrome.storage.sync.get("pages",function(val) {
-		var pages = []
-		if (val.pages) {
-			pages = val.pages
-		}
-		console.log(pages)
-		var loc = input.split("://")[1].split("#")[0];
-		if (pages.indexOf(loc) < 0) {
-			pages.push(loc)
-			console.log(chrome.storage.sync.set({"pages": pages}));
-			var a = {}
-			var arr = JSON.parse(event.data)
-			var dom = location.origin.split("://")[1].split(".");
-			arr.push(dom[dom.length-2]+":30.0");
-			var d = new Date();
-			a[loc] = [arr,title ? title[1] : input ,type,(d.getMonth()+1)+"/"+(d.getDay()+1)+"/"+(d.getYear()-100)]
-			console.log(a)
-			chrome.storage.sync.set(a);
-			/*createSnackbar("Page Saved","View Bookmarks", function() {
-				chrome.runtime.sendMessage({greeting: "bookmarks"})
-			});*/
-		} else {
-			/*createSnackbar("Page Already Saved","View Bookmarks", function() {
-				chrome.runtime.sendMessage({greeting: "bookmarks"})
-			});*/
-		}
-	})
-};
-
-	
-		}
-
-		xhr.send()
+			var worker = new Worker(chrome.runtime.getURL('crx/worker.js'));
+			var type = "s"; //allows for other types
+			var title = xhr.response.match(/<title>(.+)<\/title>/);
+			worker.postMessage(JSON.stringify([xhr.response.split("<body")[1].split("</body")[0].replace(/<[^>]*>/g, "").match(/\w+/g).join(" "),title ? title[1] : input,1.1]));
+			worker.onmessage = function(event) {
+				console.log(event.data);
+				var bms = chrome.storage.sync.get("pages",function(val) {
+					var pages = [];
+					if (val.pages) {
+						pages = val.pages
+					}
+					console.log(pages);
+					var loc = input.split("://")[1].split("#")[0];
+					if (pages.indexOf(loc) < 0) {
+						pages.push(loc);
+						console.log(chrome.storage.sync.set({"pages": pages}));
+						var a = {},
+							arr = JSON.parse(event.data),
+							dom = location.origin.split("://")[1].split("."),
+							d = new Date();
+						arr.push(dom[dom.length-2]+":30.0");
+						a[loc] = [arr,title ? title[1] : input ,type,(d.getMonth()+1)+"/"+(d.getDay()+1)+"/"+(d.getYear()-100)]
+						chrome.storage.sync.set(a);
+						/*createSnackbar("Page Saved","View Bookmarks", function() {
+						 chrome.runtime.sendMessage({greeting: "bookmarks"})
+						 });*/
+					} else {
+						/*createSnackbar("Page Already Saved","View Bookmarks", function() {
+						 chrome.runtime.sendMessage({greeting: "bookmarks"})
+						 });*/
+					}
+				})
+			};
+		};
+		xhr.send();
 		addCard(input, undefined, input);
 		bookmarkURL.val('');
 		$('#modal-add').closeModal();
@@ -149,12 +143,22 @@ tabs.children().eq(1).click(function() {
 });
 $(document).delegate('#save-settings', 'click', function() {
 	console.log(settingNumber.val());
+	if (settingAutomatic.is(':checked')) {
+		localStorage.removeItem('num');
+	} else {
+		localStorage.setItem('num', settingNumber.val());
+	}
+});
+$(document).delegate('#import-yes', 'click', function() {
+	importModal.find('.modal-content').html('<h4>Importing Bookmarks</h4>');
+	importModal.find('.progress').addClass('active');
+	$('#import-yes').addClass('disabled');
+	// Insert import code here
 });
 settingAutomatic.change(function() {
-	if(settingAutomatic.is(":checked")) {
+	if(settingAutomatic.is(':checked')) {
 		settingNumber.prop('disabled', true);
 		settingAlgorithm.prop('disabled', true);
-		localStorage.removeItem('num');
 	} else {
 		settingNumber.prop('disabled', false);
 		settingAlgorithm.prop('disabled', false);
@@ -163,7 +167,8 @@ settingAutomatic.change(function() {
 $(document).ready(function() {
 	settingAlgorithm.material_select();
 });
-settingNumber.change(function() {
-	localStorage.setItem("num", settingNumber.val())
-});
 //datePicker[0].oninput = sortyByDate(datePicker.val());
+if (localStorage.getItem('run-once') === null) {
+	importModal.openModal();
+	localStorage.setItem('run-once', true);
+}
